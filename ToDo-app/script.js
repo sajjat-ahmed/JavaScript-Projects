0e6de1c39c9677c93dbe935f0d1abd87d5fbf36d
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-11-11 19:39:49 +0600

    javascript projects

---
+++ ToDo-app/script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todos = document.getElementById('todos');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const todoText = input.value;
+
+    if(todoText) {
+        const todoE1 = document.createElement("li");
+        todoE1.innerText = todoText;
+        todos.appendChild(todoE1);
+
+        todoE1.addEventListener('click', () => {
+            todoE1.classList.toggle('complited');
+        });
+
+        todoE1.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+
+            todoE1.remove();
+        })
+
+        input.value = "";
+    }
+});
